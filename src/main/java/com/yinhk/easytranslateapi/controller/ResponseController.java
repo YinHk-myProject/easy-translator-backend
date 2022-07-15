5894e1de5830eb5a83254713741d7ae578ae411b
YinHk <>
2022-07-15 12:18:24 -0400

    change to post request handler

--- src/main/java/com/yinhk/easytranslateapi/controller/ResponseController.java
+++ src/main/java/com/yinhk/easytranslateapi/controller/ResponseController.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yinhk.easytranslateapi.pojo.InfoDto;
 import com.yinhk.easytranslateapi.pojo.TranslationDataDto;
 import com.yinhk.easytranslateapi.service.TranslationClient;
+
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -20,12 +24,17 @@ public class ResponseController {
 	@Autowired
 	private TranslationClient translationClient;
 	
-	@GetMapping("/translate")
+	//@GetMapping("/translate")
+	@PostMapping("/translate")
 	public TranslationDataDto getTranslate(
-			@RequestParam(value = "query", required = true) String q,
-			@RequestParam(value = "source", required = true) String source,
-			@RequestParam(value = "target", required = true) String target) {
-		
+			@RequestBody InfoDto infoDto
+			//@RequestParam(value = "query", required = true) String q,
+			//@RequestParam(value = "source", required = true) String source,
+			//@RequestParam(value = "target", required = true) String target
+			) {	
+		String q = infoDto.getQuery();
+		String source = infoDto.getSource();
+		String target = infoDto.getTarget();
 		return translationClient.requestTranslation(q, source, target);
 	}
 			
---
+++ src/main/java/com/yinhk/easytranslateapi/pojo/InfoDto.java
@@ -0,0 +1,31 @@
+package com.yinhk.easytranslateapi.pojo;
+
+public class InfoDto {
+	private String query;
+	private String source;
+	private String target;
+	
+	public String getQuery() {
+		return query;
+	}
+	
+	public void setQ(String query) {
+		this.query = query;
+	}
+	
+	public String getSource() {
+		return source;
+	}
+	
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	public String getTarget() {
+		return target;
+	}
+	
+	public void setTarget(String target) {
+		this.target = target;
+	}
+}
